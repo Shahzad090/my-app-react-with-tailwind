06a6e90d9eb25afad7f8ae164ec74fadf2e33fc7
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-10-16 19:41:25 +0500

    clas

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import NavbarCom from "./Layout/NavbarCom";
 import axios from "axios";
 import "./App.css";
 import "./index.css";
-import { useState } from "react"
-
+import { useState } from "react";
 
 function App() {
   const [postData, setPostData] = useState([]);
   const [showLoader, setshowLoader] = useState([true]);
   const getApiData = () => {
-    axios 
-      .get("https://jsonplaceholder.typicode.com/posts",)
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
       .then((res) => {
         console.log(res.data);
         setPostData(...res.data);
@@ -21,13 +20,12 @@ function App() {
       });
   };
 
-
-  const postApiData = () =>  
+  const postApiData = () =>
     axios
-    .post("https://jsonplaceholder.typicode.com/posts", {
-      body: "abc",
-      title: "ahj",
-      userID: 100,
+      .post("https://jsonplaceholder.typicode.com/posts", {
+        body: "abc",
+        title: "ahj",
+        userID: 100,
       })
       .then((res) => {
         console.log(res.data);
@@ -35,18 +33,36 @@ function App() {
       .catch((err) => {
         console.log(err);
       });
-    
 
-      
+  const editData = () => {
+    axios.put("https://jsonplaceholder.typicode.com/posts/1"),
+      {
+        title: "abc",
+        body: "xyz",
+      };
+  };
+
+  const deleteData = () => {
+    axios
+      .delete("https://jsonplaceholder.typicode.com/posts/1")
+      .then(() => {
+        console.log("deleted successfully");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <>
       <div>
         <NavbarCom />
-        <button onClick={ getApiData }>get data</button>
-        <button onClick={ postApiData }>post data</button>
-      </div>  
+        <button onClick={getApiData}>get data</button>
+        <button onClick={postApiData}>post data</button>
+      </div>
+        
     </>
-  )
+  );
 }
 
-export default App;   
+export default App;
