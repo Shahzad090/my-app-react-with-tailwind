47edca9912aa69a83583ff1871585d984ed8660b
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-10-20 23:42:33 +0500

    2

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       .then((res) => {
         console.log(res.data);
         setPostData(...res.data);
-        setshowLoader();
+        setshowLoader(f);
       })
       .catch((err) => {
         console.log(err);
